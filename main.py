72e4123405a3230147c9719b3f53e20731b547e6
wedenborg <emiliewedenborg@gmail.com>
2022-08-15 10:27:07 +0200

    minor updates

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+
+
 import os
 import glob
-
 import numpy as np
-
-
 import Adafruit_DHT
 import RPi.GPIO as GPIO
 from datetime import datetime
@@ -34,25 +33,18 @@ knap = 5
 led = 6
 
 mylcd = i2c_lcd_driver.lcd()
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(BlaaTemp, GPIO.OUT) # GPIO Assign mode
+GPIO.setup(BrunTemp, GPIO.OUT) # GPIO Assign mode
+GPIO.setup(Humid, GPIO.OUT) # GPIO Assign mode
+GPIO.setup(Sensor, GPIO.OUT) # GPIO Assign mode
+GPIO.setup(Fan, GPIO.OUT) # GPIO Assign mode
+GPIO.setup(led, GPIO.OUT) # GPIO Assign mode
+GPIO.setup(knap, GPIO.IN) # GPIO Assign mode
 
 
-def setup(BlaaTemp,BrunTemp,Humid,Sensor,Fan, knap,led):
-    # Function that sets up the GPIO pins of heating mat, humidifier and sensor
-    # Configures how we are describing our pin numbering
-    GPIO.setmode(GPIO.BCM)
-    # Disable Warnings
-    GPIO.setwarnings(False)
-
-    GPIO.setup(BlaaTemp, GPIO.OUT) # GPIO Assign mode
-    GPIO.setup(BrunTemp, GPIO.OUT) # GPIO Assign mode
-    GPIO.setup(Humid, GPIO.OUT) # GPIO Assign mode
-    GPIO.setup(Sensor, GPIO.OUT) # GPIO Assign mode
-    GPIO.setup(Fan, GPIO.OUT) # GPIO Assign mode
-    GPIO.setup(led, GPIO.OUT) # GPIO Assign mode
-    GPIO.setup(knap, GPIO.IN) # GPIO Assign mode
-
-    return
-
+ 
 def heatmatON(temp1,temp2):
     # Function that turns on heating mat
     GPIO.output(temp1, GPIO.LOW)
@@ -70,7 +62,7 @@ def heatmatOFF(temp1,temp2):
 def signalHumidifier(Humid):
     # Function that turns the humidifier on or off
     GPIO.output(Humid, GPIO.LOW)
-    sleep(2)
+    sleep(1)
     GPIO.output(Humid, GPIO.HIGH)
     return
 
@@ -96,7 +88,6 @@ def writeToDB(path,p):
         fileVariable.close()
     finally:
         dbData.close()
-    
     return
     
 
@@ -107,84 +98,73 @@ def storeData(tempS, temperature, humidity, path):
     out.close()
     return
 
-setup(BlaaTemp,BrunTemp,Humid,Sensor,Fan, knap, led)
-
 p = np.loadtxt("/home/emilie/Desktop/Code/FermentationBox/db.txt", delimiter=",", dtype=str)
 path = '/home/emilie/Desktop/Code/FermentationBox/test.txt'
 
-K = 60
+
+time = datetime.now()
+timeOld = time
+lastStored = time
+humidifier = 0
+GPIO.output(Fan, GPIO.LOW) # Turn on fan for better air circulation
+heatmatOFF(BlaaTemp,BrunTemp)
+mylcd.lcd_display_string("Starting...",1,0)
 KorHumid = 1
-try:
-    currentTime = datetime.now()
-    lastRun = datetime.now()
-    lastStored = datetime.now()
-    mylcd.lcd_display_string("Starting...",1,0)
-    sleep(3)
-    humidifier = False
-    GPIO.output(Fan, GPIO.LOW) # Turn on fan for better air circulation
-    while True:
-        a = GPIO.input(knap)
-        mylcd.lcd_display_string(str(a),2,15)
-        if (a == 1):
-            if humidifier == True:
+f = 20
+GPIO.output(led,GPIO.LOW) 
+while True:
+    time = datetime.now()
+    
+    knapInd = GPIO.input(knap)
+    if (knapInd == 1):
+        if (KorHumid == 1):
+            KorHumid = 0
+            GPIO.output(led,GPIO.HIGH)
+            if (humidifier == 1):
+                humidifier = 0
                 signalHumidifier(Humid)
-                humidifier = False
-
-            if KorHumid == 1:
-                KorHumid = 0
-                GPIO.output(led,GPIO.HIGH)
-            else:
-                KorHumid = 1
-                GPIO.output(led,GPIO.LOW)
-
-        tempS = stickTemp.read_temp()
+            sleep(3)
+        else:
+            KorHumid = 1
+            GPIO.output(led,GPIO.LOW)
+    
         
-        humidity, temperature = Adafruit_DHT.read_retry(11, Sensor)
-        if humidity is not None and temperature is not None:
+    tempS = stickTemp.read_temp()
+    humidity, temperature = Adafruit_DHT.read_retry(11, Sensor)
+    if humidity is not None and temperature is not None:
+        mylcd.lcd_clear()
+        mylcd.lcd_display_string("Temperature: "+str(tempS),1,0)
+        mylcd.lcd_display_string("Humidity: "+str(humidity),2,0)
+        mylcd.lcd_display_string(str(knapInd),2,15)
 
-
-            mylcd.lcd_display_string("Temperature: "+str(tempS),1,0)
-            #mylcd.lcd_display_string(str(humidifier),1,0)
-            mylcd.lcd_display_string("Humidity: "+str(humidity),2,0)
-
-
-            if abs((currentTime-lastStored).total_seconds()) >= 60:
-                storeData(tempS, temperature, humidity,path)
-                lastStored = datetime.now()
-
-            if tempS < 28:
-                heatmatON(BlaaTemp,BrunTemp)
-            elif tempS > 34:
-                heatmatOFF(BlaaTemp,BrunTemp)
-
-            currentTime = datetime.now()
-            if humidity < 70 and KorHumid == 1 and humidifier == False:
-                if abs((currentTime-lastRun).total_seconds()) >= 120:
-                    signalHumidifier(Humid)
-                    humidifier = True
-                    lastRun = currentTime
-
-            if KorHumid == 1 and humidifier == True and abs((currentTime-lastRun).total_seconds()) >= 20:
-                    signalHumidifier(Humid)
-                    humidifier == False
-                    lastRun = currentTime
-
-            if datetime.now().strftime('%H:%M') == '00:00':
-                writeToDB(path,p)
+    if abs((time-lastStored).total_seconds()) >= 60:
+        storeData(tempS, temperature, humidity,path)
+        lastStored = datetime.now()
             
+            
+    if tempS < 28:
+        heatmatON(BlaaTemp,BrunTemp)
+    elif tempS > 34:
+        heatmatOFF(BlaaTemp,BrunTemp)
+            
+            
+    if humidity < 70 and KorHumid == 1 and humidifier == 0:
+        if abs((time-timeOld).total_seconds()) >= 120:
+            signalHumidifier(Humid)
+            humidifier = 1
+            timeOld = time
 
-except KeyboardInterrupt:
-    GPIO.output(Fan, GPIO.HIGH)
-    #mylcd.lcd_clear()
-    heatmatOFF(BlaaTemp,BrunTemp)
-    if humidity < 70 and humidifier == True:
-        signalHumidifier(Humid)
+            if humidity <60:
+                f = 40
+            else: 
+                f = 20
 
-except:
-    GPIO.output(Fan, GPIO.HIGH)
-    #mylcd.lcd_clear()
-    heatmatOFF(BlaaTemp,BrunTemp)
-    if humidity < 70 and humidifier == True:
+    if KorHumid == 1 and humidifier == 1 and abs((time-timeOld).total_seconds()) >= f:
         signalHumidifier(Humid)
+        humidifier = 0
+        timeOld = time
+
+    if datetime.now().strftime('%H:%M') == '00:00':
+        writeToDB(path,p)
 
 
